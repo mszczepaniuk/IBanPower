5bbe141139d627a6cbe44bfe7922205bf4ee3f76
mszczepaniuk <asd2ws@gmail.com>
2019-12-24 19:48:37 +0100

    Added default map priority

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
-
 chrome.runtime.onInstalled.addListener(function () {
+    var defaultMaps = ["de_nuke", "de_vertigo", "de_overpass", "de_mirage", "de_cache", "de_dust2", "de_train", "de_inferno"];
+    chrome.storage.sync.set({ mapPriority: defaultMaps });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([
             {
