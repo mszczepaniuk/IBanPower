5795bc78b153cb41d52f07e3469ba70c0468a7e9
mszczepaniuk <asd2ws@gmail.com>
2019-12-24 17:49:46 +0100

    Creates table with opposing captain maps data

--- popup.js
+++ popup.js
@@ -1,11 +1,11 @@
 chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
     let matchId = getMatchIdFromURL(tabs[0].url);
-    document.getElementById("test").innerText = matchId;
     var builder = new PlayerDataApiBuilder(matchId, "asd2ws");
     var playerData = builder.build();
     console.log(playerData);
-
-
+    $(document).ajaxStop(function () {
+        createDataTable(playerData);
+    });
 });
 
 function getMatchIdFromURL(url) {
@@ -13,3 +13,15 @@ function getMatchIdFromURL(url) {
     let matchId = matchIdFormat.exec(url);
     return matchId;
 }
+// TODO: Sorting
+function createDataTable(playerData) {
+    var mapDictLength = Object.keys(playerData.totalMapRatio).length;
+    $('#dataTable').html('');
+    var tableHtmlString = `<table><tr><th>Map</th><th>TMR(${playerData.totalMatches})</th><th>RMR(${playerData.recentMatches})</th></tr>`;
+    Object.keys(playerData.totalMapRatio).forEach(x => {
+        console.log(x);
+        tableHtmlString += `<tr><td>${x}</td><td>${parseFloat(playerData.totalMapRatio[x] * 100).toFixed(2)}%</td><td>${parseFloat(playerData.recentMapRatio[x] * 100).toFixed(2)}%</td></tr>`;
+    });
+    tableHtmlString += "</table>";
+    $('#dataTable').append(tableHtmlString);
+} 
