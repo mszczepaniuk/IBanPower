a7db661c39f591591449a78fe47abad654e3f024
mszczepaniuk <asd2ws@gmail.com>
2019-12-25 04:39:27 +0100

    Sorting popup data table

--- popup.js
+++ popup.js
@@ -8,15 +8,23 @@ chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs)
             chrome.storage.sync.get(['mapPriority'], x => {
                 var playerMapPriority = createMapPriorityArray(playerData);
                 var banArrayFactory = new BanArrayFactory(x.mapPriority, playerMapPriority);
-                console.log(x.mapPriority);
-                console.log(playerMapPriority);
                 createBanTable(banArrayFactory.Create());
             });
 
         });
     });
 });
-// TODO: Error managment in general.
+
+$(document).ready(function () {
+    $(document).on('click', '.dataTableHeader', x => {
+        var id = x.target.id.replace('col', '');
+        sortDataTable(id);
+    });
+});
+
+
+
+
 function getMatchIdFromURL(url) {
     let matchIdFormat = new RegExp('.-.{8}-.{4}-.{4}-.{4}-.{12}');
     let matchId = matchIdFormat.exec(url);
@@ -25,11 +33,47 @@ function getMatchIdFromURL(url) {
     }
     return matchId;
 }
+
+function sortDataTable(columnId) {
+    var playerData = new PlayerData();
+    var mapsArray = [];
+    var htmlCollection = $("#dataTable").children().children()[0].children;
+
+    playerData.recentMatches = $(htmlCollection[0]).children()[2].innerText.replace('RMR(', '').replace(')', '');
+    playerData.totalMatches = $(htmlCollection[0]).children()[1].innerText.replace('TMR(', '').replace(')', '');
+    
+    for (var i = 1; i < htmlCollection.length; i++) {
+        var temp = [];
+        for (var j = 0; j < $(htmlCollection[i]).children().length; j++) {
+            var textInCell = $(htmlCollection[i]).children()[j].textContent;
+            if (j === 0) {
+                temp.push(textInCell);
+            }
+            else {
+                temp.push(parseFloat(textInCell.replace('%', ''))/100);
+            }
+        }
+        mapsArray.push(temp);
+    }
+    mapsArray.sort((x, y) => {
+        return y[columnId] - x[columnId];
+    });
+    var totalMapRatio = {};
+    var recentMapRatio = {};
+    mapsArray.forEach(x => {
+        totalMapRatio[x[0]] = x[1];
+        recentMapRatio[x[0]] = x[2];
+    });
+    playerData.totalMapRatio = totalMapRatio;
+    playerData.recentMapRatio = recentMapRatio;
+    createDataTable(playerData);
+}
 // TODO: Sorting.
 function createDataTable(playerData) {
+    $(`#dataTable`).html('');
     $('#info').html('');
     var tableHtmlString = 'Enemy captain map data';
-    tableHtmlString += `<table><tr><th>Map</th><th>TMR(${playerData.totalMatches})</th><th>RMR(${playerData.recentMatches})</th></tr>`;
+    tableHtmlString += `<table><tr><th id="col0" class="dataTableHeader">Map</th><th id="col1" class="dataTableHeader">TMR(${playerData.totalMatches})</th><th id="col2" class="dataTableHeader">RMR(${playerData.recentMatches})</th></tr>`;
     Object.keys(playerData.totalMapRatio).forEach(x => {
 
         tableHtmlString += `<tr><td>${x}</td><td>${parseFloat(playerData.totalMapRatio[x] * 100).toFixed(2)}%</td><td>${parseFloat(playerData.recentMapRatio[x] * 100).toFixed(2)}%</td></tr>`;
@@ -39,6 +83,7 @@ function createDataTable(playerData) {
 }
 
 function createBanTable(maps) {
+    $('#banTable').html('');
     var tableHtmlString = 'Suggested ban priority<table>';
     maps.forEach(x => {
         tableHtmlString += `<tr><td>${x}</td></tr>`;
