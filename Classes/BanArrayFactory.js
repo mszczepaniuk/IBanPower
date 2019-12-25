1105e83a4ac756e1ff0b7634b9ca1e1c90cd2af5
mszczepaniuk <asd2ws@gmail.com>
2019-12-25 13:26:49 +0100

    Changes in BanArray alghoritm

--- Classes/BanArrayFactory.js
+++ Classes/BanArrayFactory.js
@@ -8,31 +8,37 @@ class BanArrayFactory {
     }
     maps = [];
 
+    // Before proper algorith starts, first map from user priority is taken from initial arrays
+    // since at no point user will want to ban it.
+    // Algorith searches for lowest map in user priorities to be in upper half of enemy priorities. 
+    // Then it pushes it to BanArray, and removes this map from user and enemy initial map arrays.
+    // It does this until all the maps were pushed from initial map arrays.
     Create() {
-        // Algorith searches for lowest map in user priorities to be in top n enemy priorities. 
-        // Then it pushes it to to BanArray, and removes this map from user and enemy initial maps.
-        // It does this until all the maps were pushed from initial maps.
-        var n = 3;
-        while (this.userMaps.length > n) {
+        console.log(this.userMaps);
+        var lastBan = this.userMaps[0];
+        this.userMaps.splice(0, 1);
+        var lastBanIndexInEnemyMaps = this.enemyMaps.findIndex(x => {
+            return x === lastBan;
+        });
+        this.enemyMaps.splice(lastBanIndexInEnemyMaps, 1);
+
+        var n = Math.round(this.userMaps.length / 2);
+        while (this.userMaps.length > 0) {
             loop:
-            for (var i = this.userMaps.length - 1; i >= 2; i--) {
+            for (var i = this.userMaps.length - 1; i >= 0; i--) {
                 for (var j = 0; j < n; j++) {
                     if (this.userMaps[i] === this.enemyMaps[j]) {
                         this.maps.push(this.userMaps[i]);
                         this.userMaps.splice(i, 1);
                         this.enemyMaps.splice(j, 1);
-
+                        n = Math.round(this.userMaps.length / 2);
                         break loop;
                     }
                 }
             }
         }
-        for (var k = n - 1; k >= 0; k--) {
-            this.maps.push(this.userMaps[k]);
-            this.userMaps.splice(k, 1);
-        }
-        this.enemyMaps = [];
-
+        this.maps.push(lastBan);
+        console.log(this.maps);
         return this.maps;
     }
 }
